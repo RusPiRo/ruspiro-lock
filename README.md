fc4a52a130e5254c6f2fab9343031030d92a96c8
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-07-26 14:24:42 +0200

    Update Doc+Readme

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 # RusPiRo Lock crate
 
+This crates provide two simple abstractions on low level atomic locks. The ``Spinlock`` and the ``Semaphore``.
+
+# Usage
+
+To use this crate simply add the dependency to your ``Cargo.toml`` file:
+```
+[dependencies]
+ruspiro-lock = { git = 'https://github.com/RusPiRo/ruspiro-lock' }
+```
+
+Once done the definition and usage of the locks is as follows:
+
+Using a Spinlock to ensure exclusive access:
+```
+use ruspiro_lock::*;
+
+static SPIN: Spinlock = Spinlock::new();
+
+fn main() {
+    SPIN.aquire();
+    // following code is only executed if the lock could be aquired, the executing core pause till then
+    let _ = 10 + 3;
+    SPIN.release();
+}
+```
+
+Using a Semaphore to specify how often specific access is valid:
+```
+use ruspriro_lock::*;
+
+static mut SEMA: Semaphore = Semaphore::new(1);
+
+fn main() {
+    unsafe { // unsafe necessary as accessing mut static's is unsafe
+        if SEMA.try_down().is_ok() {
+            // we gained access to the semaphore, do something
+            let _ = 20 /4;
+            SEMA.up();
+        }
+    }
+}
+```
+
 ## License
 This crate is licensed with the Appache License 2.0
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,43 @@
 #![feature(asm)]
 
 //! # Atomic locks for Raspberry Pi baremetal
+//! This crate provides two options of locks. [Spinlock] and [Semaphore]. They provide mechanisms to secure cross core
+//! access to shared data like MMIO registers of peripherals. As the locks depend on low level atomics they do only work
+//! on the Raspberry Pi if the MMU is properly configured. Otherwise using either of the lock functions will hang the 
+//! core it has been used on.
+//! 
+//! # Example
+//! Using a Spinlock to ensure exclusive access.
+//! ```
+//! use ruspiro_lock::*;
+//! 
+//! static SPIN: Spinlock = Spinlock::new();
+//! 
+//! fn main() {
+//!     SPIN.aquire();
+//!     // following code is only executed if the lock could be aquired, the executing core pause till then
+//!     let _ = 10 + 3;
+//!     SPIN.release();
+//! }
+//! ```
+//! 
+//! using a Semaphore to specify how often specific access is valid
+//! 
+//! ```
+//! use ruspriro_lock::*;
+//! 
+//! static mut SEMA: Semaphore = Semaphore::new(1);
+//! 
+//! fn main() {
+//!     unsafe { // unsafe necessary as accessing mut static's is unsafe
+//!         if SEMA.try_down().is_ok() {
+//!             // we gained access to the semaphore, do something
+//!             let _ = 20 /4;
+//!             SEMA.up();
+//!         }
+//!     }
+//! }
+//! ```
 //! 
 
 pub mod spinlock;
